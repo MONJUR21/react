8e635c54f551c9e73d20521cd8c1772e1cd09626
MONJUR21 <monjur.csecu@gmail.com>
2024-03-05 01:53:41 +0600

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import { useState,useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+useEffect(() => {
+  alert("This is my alert");
+}, [])
 
 function App() {
   const [count, setCount] = useState(0)
