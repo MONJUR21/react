a01c727d9bd529ac2b7b81fa1aba7398e8ad14b2
MONJUR21 <monjur.csecu@gmail.com>
2024-07-11 06:38:44 +0800

    commited

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NavBar.css'
+const NavBar = (props) => {
+  return (
+    <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Contact Us</li>
+                <li>About Us</li>
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default NavBar
